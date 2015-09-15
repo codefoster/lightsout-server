e6911b4d331f8edd47b6c43c9050a785e55b66cd
Jeremy Foster <jeremy.foster@live.com>
2015-09-15 16:58:19 -0700

    finished for the day

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+	playerCount: 1
+}
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var config = require('./config');
+
+var clients = [];
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
@@ -8,9 +11,22 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
 	console.log('a user connected');
-	socket.on('action', function(message) {
-		//here's how we receive an 'action' socket message
-		console.log(message);			
+	clients.push({active:true,id:socket.id});
+	if(clients.length == config.playerCount) startGame();
+	socket.on('overSpeed', function() {
+		var activePlayerCount = 0;
+		var lastActivePlayer;
+		console.log(clients.length);
+		clients.forEach(function(client){
+			console.log(client.id);
+			///if(client.id == socket.id) client.active = false;
+			if(client.active) {
+				lastActivePlayer = client;
+				console
+				activePlayerCount++;
+			}
+		});
+		if(activePlayerCount == 1) endGame(lastActivePlayer);
 	});
 	
 	//here's how we send a message
@@ -22,3 +38,11 @@ app.set('port', process.env.port || 3000);
 http.listen(app.get('port'), function(){
   console.log('listening on *:' + app.get('port'));
 });
+
+function startGame(){
+	io.emit('startGame');
+}
+
+function endGame(winner){
+	io.emit('endGame',winner);
+}
